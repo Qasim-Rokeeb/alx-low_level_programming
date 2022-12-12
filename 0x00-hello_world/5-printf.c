10ec6a4268e17c507716b096bcc6ae798eb3548b
Qasim Rokeeb <qasimroqeeb>
2022-12-12 09:09:42 -0800

    work please

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - pror5g
+ * Description: use printf
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
